b9d907467da97f8015677ec95d120cad6f71d7e0
Matthias Eschhold <matthias.eschhold@novatec-gmbh.de>
2022-12-11 17:47:31 +0100

    Update VehicleToVehicleResourceMapper.cs

--- Source/Vehicle/Adapter/In/VehicleToVehicleResourceMapper.cs
+++ Source/Vehicle/Adapter/In/VehicleToVehicleResourceMapper.cs
@@ -1,6 +1,24 @@
-﻿namespace Hexacleanws.Source.Vehicle.Adapter.In
+﻿using AutoMapper;
+using Hexacleanws.Source.Vehicle.Domain.Model;
+
+namespace Hexacleanws.Source.Vehicle.Adapter.In.Web
 {
-    public class VehicleToVehicleResourceMapper
-    {
+	public class VehicleToVehicleResourceMapper : Profile
+	{
+		public VehicleToVehicleResourceMapper()
+		{
+            CreateMap<VehicleRootEntity, VehicleResource>()
+                .ReverseMap();
+
+            CreateMap<VehicleRootEntity, VehicleResource>()
+                .ForMember(dest => dest.Vin, opt => opt.MapFrom(s => s.Vin.Value))
+                .ForMember(dest => dest.SerialNumber, opt => opt.MapFrom(s => s.VehicleMasterData.SerialNumber.Value))
+                .ForMember(dest => dest.MileageUnit, opt => opt.MapFrom(s => s.VehicleMasterData.MileageUnit.Value))
+                .ForMember(dest => dest.VehicleModelDescription, opt => opt.MapFrom(s => s.VehicleMasterData.VehicleModel.ModelDescription))
+                .ForMember(dest => dest.VehicleModelName, opt => opt.MapFrom(s => s.VehicleMasterData.VehicleModel.ModelType));
+                //equipment...
+
+        }
     }
 }
+
